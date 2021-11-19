e310a07b59275743effde0a0ad9b4fac4a4a5acb
JianChengChang <david0912928@gmail.com>
2021-11-19 17:00:49 +0800

    pyqt5 practice

---
+++ qt5test/03. Event handling in QML/main.py
@@ -0,0 +1,14 @@
+import os, sys
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtQml import *
+from PyQt5.QtQuick import *
+from PyQt5.Qt import *
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    engine = QQmlApplicationEngine()
+    engine.load(QUrl.fromLocalFile(r".\03. Event handling in QML\main.qml"))
+
+    sys.exit(app.exec_())
